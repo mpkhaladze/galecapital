905276d72a697f02588df701f650a1aa5b43d528
Merab <merabpkhaladze@yahoo.com>
2017-06-09 18:05:07 +0400

    start project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <title>
+        Gale Capital — Construction Company
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="description" content="Galecapitali"/>
+    <meta name="keywords" content="galecapitali, holding company, construction company, development company, ჰოლდინგური კომპანია, სამშენებლო კომპანია, გალიკაპიტალი, გალეკაპიტალი, დეველოპერული კომპანია"/>
+    <meta name="author" content="gghughunishvili"/>
+    <meta name="google-site-verification" content="fOmGAKYLpuFt6fGvx-e6LsGfs6ASvBW5LBU4YexLIRM" />
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content=""/>
+    <meta name="twitter:image" content=""/>
+    <meta name="twitter:url" content=""/>
+    <meta name="twitter:card" content=""/>
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="shortcut icon" href="favicon.ico"/>
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600,400italic,700' rel='stylesheet' type='text/css'/>
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}"/>
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="{{ asset('css/icomoon.css') }}"/>
+    <!-- Simple Line Icons -->
+    <link rel="stylesheet" href="{{ asset('css/simple-line-icons.css') }}"/>
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}"/>
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}"/>
+    <!-- Styleswitcher ( This style is for demo purposes only, you may delete this anytime. ) -->
+    <link rel="stylesheet" id="theme-switch" href="{{ asset('css/style.css') }}"/>
+    <!-- Modernizr JS -->
+    <script src="{{ asset('js/modernizr-2.6.2.min.js') }}"></script>
+</head>
+<body>
+    <header role="banner" id="fh5co-header">
+        <div class="container">
+            <!-- <div class="row"> -->
+            <nav class="navbar navbar-default">
+                <div class="navbar-header">
+                    <!-- Mobile Toggle Menu Button -->
+                    <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <i>
+                        </i>
+                    </a>
+                    <a class="navbar-brand" href="{{url('/')}}">
+                        Gale Capital
+                    </a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active">
+                            <a href="#" data-nav-section="მთავარი">
+                                <span>
+                                        მთავარი
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" data-nav-section="about">
+                                <span>
+                                    
+                                        ჩვენ შესახებ
+                                    
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" data-nav-section="soon">
+                                <span>
+                                        წინადადებები
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" data-nav-section="pricing">
+                                <span>
+                                        გაყიდვები
+                                    
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" data-nav-section="contacts">
+                                <span>
+                                    
+                                        კონტაქტი
+                                    
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- </div> -->
+        </div>
+    </header>
+        
+    @yield('content')
+    
+    <div id="fh5co-testimonials" data-section="contacts">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 section-heading text-center">
+                    <h2 class="to-animate">
+                    
+                            კონტაქტი
+                        
+                    </h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h1 class="custom-contact-text">
+                        41 Vazha-Pshavela ave. 0177 Tbilisi, Georgia. LTD GALE CAPITAL
+                    </h1>
+                    <h1 class="custom-contact-text">
+                        შპს გალე კაპიტალი, ვაჟა ფშაველას გამზ. 41, თბილისი 0177, საქართველო.
+                    </h1>
+                    <h1 class="custom-contact-text">
+                        <i class="glyphicon glyphicon-envelope">
+                        </i>
+                        <span>
+                            galecomge@gmail.com
+                        </span>
+                    </h1>
+                </div>
+                <div class="col-sm-6">
+                    <h1>
+                        <a href="tel:+995597503434" class="contact_tel_wrapper">
+                            <i class="glyphicon glyphicon-earphone">
+                            </i>
+                        </a>
+                        <span class="contact_number_style">
+                            +995597503434
+                        </span>
+                    </h1>
+                    <h1>
+                        <a href="tel:+995577193223" class="contact_tel_wrapper">
+                            <i class="glyphicon glyphicon-earphone">
+                            </i>
+                        </a>
+                        <span class="contact_number_style">
+                            +995577193223
+                        </span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="map_wrapper">
+        <div id="map"></div>
+    </div>
+    <footer id="footer" role="contentinfo">
+        <div class="container">
+            <div class="row row-bottom-padded-sm">
+                <div class="col-md-12">
+                    <p class="copyright text-center">
+                        &copy; 2017
+                        <a href="index.html">
+                            Galecapital LTD
+                        </a>
+                        . All Rights Reserved.
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <ul class="social social-circle">
+                            <li><a href="#"><i class="icon-twitter"></i></a></li>
+                            <li>
+                                <a href="https://www.facebook.com/galecomge">
+                                    <i class="icon-facebook">
+                                    </i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.youtube.com/channel/UCkddkl-CXiubFkj0at9-FYA">
+                                    <i class="icon-youtube">
+                                    </i>
+                                </a>
+                            </li>
+                            <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- jQuery -->
+        <script src="{{ asset('js/jquery.min.js') }}"></script>
+        <!-- jQuery Easing -->
+        <script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+        <!-- Bootstrap -->
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+        <!-- Waypoints -->
+        <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+        <!-- Owl Carousel -->
+        <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+        <!-- Main JS (Do not remove) -->
+        <script src="{{ asset('js/main.js') }}"></script>
+        <script>
+            $(document).ready(function(){
+               $('#sum_price_calc1').text($('#num_of_mkv1').text()*$('#price_of_mkv1').text());
+               $('#sum_price_calc2').text($('#num_of_mkv2').text()*$('#price_of_mkv2').text());
+               $('#sum_price_calc3').text($('#num_of_mkv3').text()*$('#price_of_mkv3').text());
+                $('#sum_price_calc4').text($('#num_of_mkv4').text()*$('#price_of_mkv4').text());
+                $('#sum_price_calc5').text($('#num_of_mkv5').text()*$('#price_of_mkv5').text());
+            });
+        </script>
+        <script src="https://maps.googleapis.com/maps/api/js" async defer></script>
+        <script >
+
+            function initMap() {
+                var myLatLng = {lat: 41.725812, lng: 44.746199};
+
+                var map = new google.maps.Map(document.getElementById('map'), {
+                  zoom: 17,
+                  center: myLatLng,
+                  mapTypeId: google.maps.MapTypeId.HYBRID
+                });
+                 map.setTilt(45);
+
+                var marker = new google.maps.Marker({
+                  position: myLatLng,
+                  map: map,
+                  title: 'Galecapitali'
+                });
+            }
+
+            $(document).ready(function () {
+
+                // you want to enable the pointer events only on click;
+
+                $('#map').addClass('scrolloff'); // set the pointer events to none on doc ready
+                $('#map_wrapper').on('click', function () {
+
+                    $('#map').removeClass('scrolloff'); // set the pointer events true on click
+                });
+
+                // you want to disable pointer events when the mouse leave the canvas area;
+
+                $("#map").mouseleave(function () {
+                    $('#map').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
+                });
+            });
+        </script>
+        <script src="https://maps.googleapis.com/maps/api/js?callback=initMap" async defer></script>
+</body>
+</html>
---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,274 @@
+@extends('layouts.app')
+@section('content')
+    <div id="slider" data-section="home">
+            <div class="owl-carousel-fullwidth">
+                <!-- You may change the background color here.  -->
+                <div class="item purple-background" style="height: 100vh; position:relative;">
+                    <div class="main_text_for_home saving">
+                            მშენებლობა და ინვესტირება გონივრულ დროსა და სივრცეში ...
+                        <span>.</span><span>.</span><span>.</span>
+                    </div>
+                    <div class="social_links_wrapper text-center">
+                        <ul class="social social-circle">
+                            <li><a href="#"><i class="icon-twitter"></i></a></li>
+                            <li>
+                                <a href="https://www.facebook.com/galecomge">
+                                    <i class="icon-facebook">
+                                    </i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="icon-youtube"></i>
+                                </a>
+                            </li>
+                            <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div id="fh5co-about-us" data-section="about">
+            <div class="container">
+                <div class="row row-bottom-padded-lg" id="about-us">
+                    <div class="col-md-12 section-heading text-center">
+                        <h2 class="to-animate">ჩვენ შესახებ</h2>
+                        <div class="row">
+                            <div class="col-md-8 col-md-offset-2 to-animate">
+                                <h3>
+                                        კომპანია, რომელიც გთავაზობთ საუკეთესო გადაწყვეტას, გონივრულ დროსა და სივრცეში
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8 to-animate">
+                        <img style="" src="images/staka3.jpg" class="img-responsive img-rounded" alt="Free HTML5 Template"/>
+                    </div>
+                    <div class="col-md-4 to-animate about_us_wrapper">
+                            <p>
+                                საქართველოს მასშტაბით ვახორციელებთ, როგორც ინდივიდუალური სახლების, აგრეთვე კორპორატიული მულტი-ფუნქციური საცხოვრებელი ეზოებისა და მრავალბინიანი კორპუსების დაგეგმარებას და მშენებლობას;
+                            </p>
+                            <p>
+                                ინდივიდუალური სახლი - მოიცავს, როგორც ძველი სახლების რეაბილიტაციას და რემონტს, აგრეთვე ახალი სახლების პროექტირებას, მშენებლობას და რემონტს;
+                            </p>
+                            <p>
+                                კორპორატიული მულტი-ფუნქციური ეზო - შედგება, როგორც ინდივიდუალური სახლებისგან, აგრეთვე მცირესართულიანი მრავალბინიანი შენობებისგან ან მარალსართულიანი კორპუსებისგან.
+                            </p>
+                            <p>
+                               მრავალბინიანი კორპუსები - ვახორციელებთ სრულ დაგეგმარებასა და შესრულებას წინასწარ გაწერილი პროცედურების შესაბამისად. აგრეთვე ცალკეული სახით ჩართულობას, მონოლითური კარკასის მშენებლობას, საფასადე და შიდა სარემონტო სამუშაოებს.
+                            </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="fh5co-testimonials" class="purple-background" data-section="soon">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 section-heading text-center">
+                        <h2 class="to-animate">მიმდინარე საკითხები </h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="box-testimony to-animate">
+                            <blockquote>
+                                <span class="quote">
+                                    <span>
+                                        <i class="icon-quote-left">
+                                        </i>
+                                    </span>
+                                </span>
+                                <p>&ldquo;საინვესტიციო პროექტების მოძიება&rdquo;</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="box-testimony to-animate">
+                            <blockquote>
+                                <span class="quote">
+                                    <span>
+                                        <i class="icon-quote-left">
+                                        </i>
+                                    </span>
+                                </span>
+                                <p>
+                                        &ldquo;დამკვეთის სურვილის მიხედვით, ჩავერთვებით მიწის მოძიების ეტაპზე&rdquo;
+                                </p>
+                            </blockquote>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="box-testimony to-animate">
+                            <blockquote>
+                                <span class="quote">
+                                    <span>
+                                        <i class="icon-quote-left">
+                                        </i>
+                                    </span>
+                                </span>
+                                <p>                              
+                                        &ldquo;გადახდა ეტაპობრივად, შესრულებული სამუშაოების მიხედვით&rdquo;       
+                                </p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="fh5co-pricing" data-section="pricing">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 section-heading text-center">
+                    <h2 class="single-animate animate-pricing-1">გაყიდვები</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="price-box to-animate popular">
+                        <h2 class="pricing-plan">
+                            <span id="num_of_mkv2">100</span>
+                            <span>მ</span><sup>2</sup>
+                        </h2>
+                        <div class="price">
+                            <sup class="currency">$</sup>
+                            <span id="price_of_mkv2">189</span>
+                            <small>
+                                <span>/ მ</span><sup>2</sup>
+                                <span>-დან</span>
+                            </small>
+                        </div>
+                        <p> შავი კარკასი (სახლი)</p>
+                        <hr/>
+                        <ul class="pricing-info">
+                            <li>პროექტირება/ნებართვა</li>
+                            <li> მონოლითი და კედლები</li>
+                            <li>გადახურვა</li>
+                            <li>კომუნიკაციები</li>
+                        </ul>
+                        <hr/>
+                        <ul class="pricing-info">
+                            <li>
+                                სულ ფასი: $
+                                <span id="sum_price_calc2"></span>
+                                <span>-დან</span>
+                            </li>
+                        </ul>
+                        <a href="#" data-nav-section="contacts" class="btn btn-primary btn-sm"> დაგვიკავშირდით
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 to-animate">
+                    <div class="price-box to-animate popular">
+                        <h2 class="pricing-plan">
+                            <span id="num_of_mkv3">100</span> 
+                            <span>მ</span><sup>2</sup>
+                        </h2>
+                        <div class="price">
+                            <sup class="currency">
+                                $
+                            </sup>
+                            <span id="price_of_mkv3">
+                                229
+                            </span>
+                            <small>
+                                / მ
+                                <sup>2</sup>              
+                                <span>-დან</span>
+                            </small>
+                        </div>
+                        <p>თეთრი კარკასი (სახლი) </p>
+                        <hr/>
+                        <ul class="pricing-info">
+                            <li> შიდა-გარე ლესვა</li>
+                            <li>კარ-ფანჯრები </li>
+                            <li style="font-size: 14px">შიდა ქსელები წერტილებად</li>
+                            <li>იატაკის მოჭიმვა </li>
+                        </ul>
+                        <hr/>
+                        <ul class="pricing-info">
+                            <li>სულ ფასი: $
+                                <span id="sum_price_calc3"></span>
+                                <span>-დან</span>
+                            </li>
+                        </ul>
+                        <a href="#" data-nav-section="contacts" class="btn btn-primary btn-sm">
+                            დაგვიკავშირდით
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="price-box to-animate popular">
+                        <h2 class="pricing-plan">
+                            <span id="num_of_mkv4">1000</span>
+                            <span>მ</span><sup>2</sup>
+                        </h2>
+                        <div class="price">
+                            <sup class="currency">
+                                $
+                            </sup>
+                            <span id="price_of_mkv4">299</span>
+                            <small>
+                                <span>/ მ</span><sup>2 </sup>
+                                <span>-დან</span>
+                            </small>
+                        </div>
+                        <p>შავი (კორპორატიული)</p>
+                        <hr/>
+                        <ul class="pricing-info">
+                            <li>პროექტირება/ნებართვა</li>
+                            <li>ლიფტი და ფასადი</li>
+                            <li>კარ-ფანჯრები</li>
+                            <li>კომუნიკაციები</li>
+                        </ul>
+                        <hr/>
+                        <ul class="pricing-info">
+                            <li> სულ ფასი: $
+                                <span id="sum_price_calc4"></span>
+                                <span>-დან</span>
+                            </li>
+                        </ul>
+                        <a href="#" data-nav-section="contacts" class="btn btn-primary btn-sm">
+                                დაგვიკავშირდით
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="price-box to-animate popular">
+                        <h2 class="pricing-plan">
+                            <span id="num_of_mkv5">1000</span>
+                            <span>მ</span><sup>2</sup>
+                        </h2>
+                        <div class="price">
+                            <sup class="currency">
+                                $
+                            </sup>
+                            <span id="price_of_mkv5">349</span>
+                            <small> / მ
+                                <sup>2</sup>
+                                <span>-დან</span>
+                            </small>
+                        </div>
+                        <p>თეთრი (კორპორატიული)</p>
+                        <hr/>
+                        <ul class="pricing-info">
+                            <li>ლესვა</li>
+                            <li>ლიფტი და ფასადი</li>
+                            <li>შიდა ქსელები</li>
+                            <li>იატაკის მოჭიმვა</li>
+                        </ul>
+                        <hr/>
+                        <ul class="pricing-info">
+                            <li>სულ ფასი: $
+                                <span id="sum_price_calc5"></span>
+                                <span>-დან</span>
+                            </li>
+                        </ul>
+                        <a href="#" data-nav-section="contacts" class="btn btn-primary btn-sm">დაგვიკავშირდით
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
