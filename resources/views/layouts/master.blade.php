359cb0c6d3516494181a62f019c51cc796644ef1
Merab <merabpkhaladze@yahoo.com>
2017-07-02 12:26:59 +0400

    changed google analitics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -250,7 +250,7 @@
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
           })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-100796999-1', 'auto');
+          ga('create', 'UA-101971096-1', 'auto');
           ga('send', 'pageview');
 
         </script>
