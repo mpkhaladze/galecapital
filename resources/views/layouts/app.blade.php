942a745f995f364832abde40e9df8b4eb146f704
Merab <merabpkhaladze@yahoo.com>
2017-07-02 10:29:18 +0400

    changed admin page interface

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,15 +63,9 @@
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
                     @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
+                        <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+
                     @endif
                 </ul>
             </div>
